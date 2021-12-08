5b68bc97120e4b7424a60ca4e92626a8b2dd017f
bswarna04 <bswarna04@gmail.com>
2021-12-08 17:24:38 -0500

    JavaCoreConcepts

---
+++ src/com/company/Main.java
@@ -0,0 +1,61 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*Scanner input = new Scanner(System.in);
+        System.out.print("Enter three words separated by spaces: ");
+        String s1 = input.next();
+        String s2 = input.next();
+        String s3 = input.next();
+        System.out.println("s1 is " + s1);
+        System.out.println("s2 is " + s2);
+        System.out.println("s3 is " + s3);*/
+
+        /* *//*     switch
+        String result= null;
+        int score;
+        switch(score=60)
+        {
+            case 90: result= "A";
+            break;
+            case 80: result= "B";
+                break;
+            case 70: result= "C";
+                break;
+            default:
+                result="F";
+
+        }
+        System.out.println(result);
+    }*//*
+//while loop
+        int i = 1;
+       *//*while (i <= 15) {
+            System.out.println("loop" + " " + i);
+            i++;
+
+        }*/
+
+    // do while
+
+       /* do {
+
+            System.out.println("DoWhileloop" + " " + i);
+            i++;
+
+        }  while (i <= 15);*/
+   //for loop
+        int i,j;
+    for(i=1;i<=5;i++) {
+        System.out.println(" outerloop" + " " + i);
+
+
+        for (j = 1; j <= 2; j++) {
+            System.out.println(" Innerloop" + " " + j);
+
+        }
+    }
+    }
+    }
